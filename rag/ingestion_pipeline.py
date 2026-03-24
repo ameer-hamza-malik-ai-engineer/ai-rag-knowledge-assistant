029db9b98c2d7de37e7794a3b531d18a60463d2a
malikf9 <157067944+malikf9@users.noreply.github.com>
2026-03-24 23:29:40 +0500

    Environment File Enable

--- rag/ingestion_pipeline.py
+++ rag/ingestion_pipeline.py
@@ -16,22 +16,40 @@ def load_documents(docs_path = "docs"):
         raise FileNotFoundError(f"Directory '{docs_path}' does not exist.")
 
 
-    loader = DirectoryLoader(path = docs_path, glob = "*.txt", show_progress=True, silent_errors=True, loader_cls=TextLoader)
+    loader = DirectoryLoader(path = docs_path, glob = "*.txt", loader_cls=TextLoader, loader_kwargs={"encoding": "utf-8"})
 
     documents = loader.load()
 
     if len(documents) == 0:
         raise ValueError(f"No documents found in directory '{docs_path}' with glob pattern '*.txt'.")
 
-    for i, doc in enumerate(documents):
-        print(f"Document {i}: {doc.metadata['source']} (length: {len(doc.page_content)} characters)")
-        print(f"Content preview: {doc.page_content[:100]}...")  # Print the first 200 characters of the content
+    # for i, doc in enumerate(documents):
+    #     print(f"Document {i}: {doc.metadata['source']} (length: {len(doc.page_content)} characters)")
+    #     print(f"Content preview: {doc.page_content[:100]}...")  # Print the first 100 characters of the content
     
     return documents
+
+def split_documents(documents, chunk_size=800, chunk_overlap=0):
+    text_splitter = CharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    split_docs = text_splitter.split_documents(documents)
+
+    # for i, doc in enumerate(split_docs):
+    #     print(f"Chunk {i}: {doc.metadata['source']} (length: {len(doc.page_content)} characters)")
+    #     print(f"Content preview: {doc.page_content[:100]}...")  # Print the first 100 characters of the content
+
+    return split_docs
+
+def create_vector_store(split_docs, collection_name="db/chroma_db"):
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+    vector_store = Chroma.from_documents(documents=split_docs, embedding=embeddings, persist_directory=collection_name, collection_metadata={"hnsw:space": "cosine"})
+    return vector_store
 
 
 def main():
     documents = load_documents()
+    split_docs = split_documents(documents)
+    vector_store = create_vector_store(split_docs)
+
 
 if __name__ == "__main__":
     main()
